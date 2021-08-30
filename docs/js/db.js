3e6e3becb4925ebdad95ee12e18040fe6142ae86
luolingchun <luolingchun@outlook.com>
2021-08-30 09:45:47 +0800

    update

---
+++ docs/js/db.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?232e9ef21b9f69b563661b2c1adc416e";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
