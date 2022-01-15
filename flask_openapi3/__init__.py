6c6bcaf4ab409910ee5bea5fa2f24305148194ba
luolingchun <luolingchun@outlook.com>
2022-01-15 15:07:12 +0800

    update __version__

--- flask_openapi3/__init__.py
+++ flask_openapi3/__init__.py
@@ -2,24 +2,31 @@
 # @Author  : llc
 # @Time    : 2021/4/30 10:14
 
+__version__ = '1.0.1'
 
 from .models.file import FileStorage
 from .models.info import Info
 from .models.oauth import OAuthConfig
 from .models.security import HTTPBase, HTTPBearer, OAuth2, APIKey, OpenIdConnect
-from .models.tag import Tag
 from .models.server import Server, ServerVariable
+from .models.tag import Tag
 from .models.validation_error import UnprocessableEntity
 from .openapi import APIBlueprint
 from .openapi import OpenAPI
 
-print(r"""
-  __ _           _                                             _  _____
- / _| |         | |                                           (_)|____ |
-| |_| | __ _ ___| | ________ ___  _ __   ___ _ __   __ _ _ __  _     / /
-|  _| |/ _` / __| |/ /______/ _ \| '_ \ / _ \ '_ \ / _` | '_ \| |    \ \
-| | | | (_| \__ \   <      | (_) | |_) |  __/ | | | (_| | |_) | |.___/ /
-|_| |_|\__,_|___/_|\_\      \___/| .__/ \___|_| |_|\__,_| .__/|_|\____/
-                                 | |                    | |
-                                 |_|                    |_|
+print(rf"""
+         __ _           _    
+        / _| |         | |   
+       | |_| | __ _ ___| | __
+       |  _| |/ _` / __| |/ /
+       | | | | (_| \__ \   <        _  _____
+       |_| |_|\__,_|___/_|\_\      (_)|____ |
+  ___  _ __   ___ _ __   __ _ _ __  _     / /
+ / _ \| '_ \ / _ \ '_ \ / _` | '_ \| |    \ \
+| (_) | |_) |  __/ | | | (_| | |_) | |.___/ /
+ \___/| .__/ \___|_| |_|\__,_| .__/|_|\____/
+      | |                    | |
+      |_|                    |_|
+        
+version: {__version__}
 """)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 from setuptools import setup, find_packages
 
-__version__ = '1.0.0'
+from flask_openapi3 import __version__
 
 long_description = open('README.md', 'r', encoding='utf-8').read()
 
