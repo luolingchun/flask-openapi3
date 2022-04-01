6f0f3938cb1ad6fe8edc965455e36fb7328d26e2
luolingchun <luolingchun@outlook.com>
2022-04-01 16:59:50 +0800

    split openapi.py

--- flask_openapi3/api_blueprint.py
+++ flask_openapi3/api_blueprint.py
@@ -1,201 +1,57 @@
 # -*- coding: utf-8 -*-
 # @Author  : llc
-# @Time    : 2021/4/30 14:25
-import json
-import os
+# @Time    : 2022/4/1 16:54
 from copy import deepcopy
 from functools import wraps
-from io import StringIO
-from typing import Optional, List, Dict, Union, Any, Type, Callable, Tuple
+from typing import Optional, List, Dict, Any, Type, Callable, Tuple
 
-from flask import Flask, Blueprint, render_template, make_response
+from flask import Blueprint
 from flask.wrappers import Response
 from pydantic import BaseModel
 
-from .api_blueprint import APIBlueprint
 from .do_wrapper import _do_wrapper
 from .http import HTTPMethod
-from .markdown import openapi_to_markdown
-from .models import Info, APISpec, Tag, Components, Server
-from .models.common import Reference, ExternalDocumentation
-from .models.oauth import OAuthConfig
-from .models.security import SecurityScheme
+from .models import Tag, Components
 from .utils import get_openapi_path, get_operation, get_responses, parse_and_store_tags, parse_parameters, \
     validate_responses_type, parse_method, get_operation_id_for_path
 
 
-class OpenAPI(Flask):
+class APIBlueprint(Blueprint):
     def __init__(
             self,
+            name: str,
             import_name: str,
             *,
-            info: Optional[Info] = None,
-            security_schemes: Optional[Dict[str, Union[SecurityScheme, Reference]]] = None,
-            oauth_config: Optional[OAuthConfig] = None,
-            responses: Optional[Dict[str, Type[BaseModel]]] = None,
+            abp_tags: Optional[List[Tag]] = None,
+            abp_security: Optional[List[Dict[str, List[str]]]] = None,
+            abp_responses: Optional[Dict[str, Type[BaseModel]]] = None,
             doc_ui: bool = True,
-            doc_expansion: str = "list",
-            doc_prefix: str = "/openapi",
-            api_doc_url: str = "/openapi.json",
-            swagger_url: str = "/swagger",
-            redoc_url: str = "/redoc",
-            rapidoc_url: str = "/rapidoc",
-            servers: Optional[List[Server]] = None,
             **kwargs: Any
     ) -> None:
         """
-        Based on Flask. Provide REST api, swagger-ui and redoc.
+        Based on Flask Blueprint
 
         Arguments:
-            import_name: just flask import_name
-            info: see https://spec.openapis.org/oas/v3.0.3#info-object
-            security_schemes: see https://spec.openapis.org/oas/v3.0.3#security-scheme-object
-            oauth_config: OAuth 2.0 configuration,
-                          see https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/oauth2.md
-            responses: OpenAPI response model
+            name: The name of the blueprint. Will be prepended to each endpoint name.
+            import_name: The name of the blueprint package, usually
+                         ``__name__``. This helps locate the ``root_path`` for the blueprint.
+            abp_tags: APIBlueprint tags for every api
+            abp_security: APIBlueprint security for every api
+            abp_responses: APIBlueprint response model
             doc_ui: add openapi document UI(swagger and redoc). Defaults to True.
-            doc_expansion: String=["list"*, "full", "none"].
-                          Controls the default expansion setting for the operations and tags.
-                          It can be 'list' (expands only the tags),
-                         'full' (expands the tags and operations) or 'none' (expands nothing).
-                         see https://github.com/swagger-api/swagger-ui/blob/master/docs/usage/configuration.md
-            doc_prefix: URL prefix used for OpenAPI document and UI. Defaults to '/openapi'.
-            api_doc_url: The OpenAPI Spec documentation. Defaults to '/openapi.json'.
-            swagger_url: The Swagger UI documentation. Defaults to `/swagger`.
-            redoc_url: The Redoc UI documentation. Defaults to `/redoc`.
-            rapidoc_url: The RapiDoc UI documentation. Defaults to `/rapidoc`.
-            servers: An array of Server Objects, which provide connectivity information to a target server.
-            kwargs: Flask kwargs
+            kwargs: Flask Blueprint kwargs
         """
-        super(OpenAPI, self).__init__(import_name, **kwargs)
-
-        self.openapi_version = "3.0.3"
-        if info is None:
-            info = Info(title='OpenAPI', version='1.0.0')
-        assert isinstance(info, Info), f"Info is required (got type {type(info)})"
-        self.info = info
-        self.security_schemes = security_schemes
-        self.responses = responses or {}
+        super(APIBlueprint, self).__init__(name, import_name, **kwargs)
         self.paths = dict()
         self.components_schemas = dict()
         self.components = Components()
         self.tags = []
         self.tag_names = []
-        self.doc_prefix = doc_prefix
-        self.api_doc_url = api_doc_url
-        self.swagger_url = swagger_url
-        self.redoc_url = redoc_url
-        self.rapidoc_url = rapidoc_url
-        if oauth_config:
-            if not isinstance(oauth_config, OAuthConfig):
-                raise TypeError("`initOAuth` must be `OAuthConfig`")
-        self.oauth_config = oauth_config
-        if doc_ui:
-            self.init_doc()
-        self.doc_expansion = doc_expansion
-        self.severs = servers
 
-    def init_doc(self) -> None:
-        """
-        Provide Swagger UI, Redoc and Rapidoc
-        """
-        _here = os.path.dirname(__file__)
-        template_folder = os.path.join(_here, 'templates')
-        static_folder = os.path.join(template_folder, 'static')
-
-        blueprint = Blueprint(
-            'openapi',
-            __name__,
-            url_prefix=self.doc_prefix,
-            template_folder=template_folder,
-            static_folder=static_folder
-        )
-        blueprint.add_url_rule(
-            rule=self.api_doc_url,
-            endpoint='api_doc',
-            view_func=lambda: self.api_doc
-        )
-        blueprint.add_url_rule(
-            rule=self.swagger_url,
-            endpoint='swagger',
-            view_func=lambda: render_template(
-                "swagger.html",
-                api_doc_url=self.api_doc_url.lstrip('/'),
-                doc_expansion=self.doc_expansion,
-                oauth_config=self.oauth_config.dict() if self.oauth_config else None
-            )
-        )
-        blueprint.add_url_rule(
-            rule=self.redoc_url,
-            endpoint='redoc',
-            view_func=lambda: render_template(
-                "redoc.html",
-                api_doc_url=self.api_doc_url.lstrip('/')
-            )
-        )
-        blueprint.add_url_rule(
-            rule=self.rapidoc_url,
-            endpoint='rapidoc',
-            view_func=lambda: render_template(
-                "rapidoc.html",
-                api_doc_url=self.api_doc_url.lstrip('/')
-            )
-        )
-        blueprint.add_url_rule(
-            rule='/markdown',
-            endpoint='markdown',
-            view_func=lambda: self.export_to_markdown()
-        )
-        blueprint.add_url_rule(
-            rule='/',
-            endpoint='index',
-            view_func=lambda: render_template(
-                "index.html",
-                swagger_url=self.swagger_url.lstrip('/'),
-                redoc_url=self.redoc_url.lstrip('/'),
-                rapidoc_url=self.rapidoc_url.lstrip('/')
-            )
-        )
-        self.register_blueprint(blueprint)
-
-    def export_to_markdown(self) -> Response:
-        """Experimental"""
-        md = StringIO()
-
-        md.write(openapi_to_markdown(self.api_doc))
-
-        r = make_response(md.getvalue())
-        r.headers['Content-Disposition'] = 'attachment; filename=api.md'
-
-        return r
-
-    @property
-    def api_doc(self) -> Dict:
-        """Generate spec json"""
-        spec = APISpec(
-            openapi=self.openapi_version,
-            info=self.info,
-            servers=self.severs,
-            externalDocs=ExternalDocumentation(
-                url=f'{self.doc_prefix}/markdown',
-                description='Export to markdown'
-            )
-        )
-        spec.tags = self.tags or None
-        spec.paths = self.paths
-        self.components.schemas = self.components_schemas
-        self.components.securitySchemes = self.security_schemes
-        spec.components = self.components
-        return json.loads(spec.json(by_alias=True, exclude_none=True))
-
-    def register_api(self, api: APIBlueprint) -> None:
-        """Register APIBlueprint"""
-        for tag in api.tags:
-            if tag.name not in self.tag_names:
-                self.tags.append(tag)
-        self.paths.update(**api.paths)
-        self.components_schemas.update(**api.components_schemas)
-        self.register_blueprint(api)
+        self.abp_tags = abp_tags or []
+        self.abp_security = abp_security or []
+        self.abp_responses = abp_responses or {}
+        self.doc_ui = doc_ui
 
     def _do_decorator(
             self,
@@ -229,27 +85,29 @@ class OpenAPI(Flask):
         :param responses: response model
         :param extra_responses: extra response dict
         :param security: security name
-        :param deprecated: mark as deprecated support. Default to not True.
         :param doc_ui: add openapi document UI(swagger and redoc). Defaults to True.
+        :param deprecated: mark as deprecated support. Default to not True.
         :param operation_id: unique string used to identify the operation.
         :param method: api method
         :return:
         """
-        if doc_ui is True:
+        if self.doc_ui is True and doc_ui is True:
             if responses is None:
                 responses = {}
             if extra_responses is None:
                 extra_responses = {}
             validate_responses_type(responses)
-            validate_responses_type(self.responses)
+            validate_responses_type(self.abp_responses)
             validate_responses_type(extra_responses)
             # global response combine api responses
-            combine_responses = deepcopy(self.responses)
+            combine_responses = deepcopy(self.abp_responses)
             combine_responses.update(**responses)
             # create operation
             operation = get_operation(func, summary=summary, description=description)
             # add security
-            operation.security = security
+            if security is None:
+                security = []
+            operation.security = security + self.abp_security or None
             # only set `deprecated` if True otherwise leave it as None
             if deprecated:
                 operation.deprecated = True
@@ -259,6 +117,7 @@ class OpenAPI(Flask):
             else:
                 operation.operationId = get_operation_id_for_path(name=func.__name__, path=rule, method=method)
             # store tags
+            tags = tags + self.abp_tags if tags else self.abp_tags
             parse_and_store_tags(tags, self.tags, self.tag_names, operation)
             # parse parameters
             header, cookie, path, query, form, body = \
@@ -266,6 +125,10 @@ class OpenAPI(Flask):
             # parse response
             get_responses(combine_responses, extra_responses, self.components_schemas, operation)
             uri = get_openapi_path(rule)
+            # merge url_prefix and uri
+            uri = self.url_prefix.rstrip("/") + "/" + uri.lstrip("/") if self.url_prefix else uri
+            # strip the right slash
+            uri = uri.rstrip('/')
             # parse method
             parse_method(uri, method, self.paths, operation)
             return header, cookie, path, query, form, body, combine_responses
---
+++ flask_openapi3/do_wrapper.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+# @Author  : llc
+# @Time    : 2022/4/1 16:54
+from typing import Dict, Any, Type, Callable
+
+from flask import request, make_response, current_app
+from flask.wrappers import Response
+from pydantic import ValidationError, BaseModel
+
+from .utils import validate_response
+
+
+def _do_wrapper(
+        func: Callable,
+        *,
+        responses: Dict[str, Type[BaseModel]] = None,
+        header: Type[BaseModel] = None,
+        cookie: Type[BaseModel] = None,
+        path: Type[BaseModel] = None,
+        query: Type[BaseModel] = None,
+        form: Type[BaseModel] = None,
+        body: Type[BaseModel] = None,
+        **kwargs: Any
+) -> Response:
+    """
+    Validate requests and responses
+    :param func: view func
+    :param responses: response model
+    :param header: header model
+    :param cookie: cookie model
+    :param path: path model
+    :param query: query model
+    :param form: form model
+    :param body: body model
+    :param kwargs: path parameters
+    :return:
+    """
+    # validate header, cookie, path and query
+    request_kwargs = dict()
+    try:
+        if header:
+            request_headers = dict(request.headers) if request.headers is not None else {}
+            for key, value in header.__annotations__.items():
+                ket_title = key.title()
+                # add original key
+                if ket_title in request_headers.keys():
+                    request_headers[key] = request_headers[ket_title]
+            request_kwargs.update({"header": header(**request_headers)})
+        if cookie:
+            request_cookies = cookie(**request.cookies if request.cookies is not None else {})
+            request_kwargs.update({"cookie": request_cookies})
+        if path:
+            request_path = path(**kwargs)
+            request_kwargs.update({"path": request_path})
+        if query:
+            request_args = request.args
+            query_dict = {}
+            for k, v in query.schema().get('properties', {}).items():
+                if v.get('type') == 'array':
+                    value = request_args.getlist(k)
+                else:
+                    value = request_args.get(k)
+                if value is not None:
+                    query_dict[k] = value
+            request_kwargs.update({"query": query(**query_dict)})
+        if form:
+            request_form = request.form
+            request_files = request.files
+            form_dict = {}
+            for k, v in form.schema().get('properties', {}).items():
+                if v.get('type') == 'array':
+                    items = v.get('items')
+                    if items is not None and items.get('type') == 'string' and items.get('format') == 'binary':
+                        # List[FileStorage]
+                        # {'title': 'Files', 'type': 'array', 'items': {'format': 'binary', 'type': 'string'}
+                        value = request_files.getlist(k)
+                    else:
+                        # List[str], List[int] ...
+                        # {'title': 'Files', 'type': 'array', 'items': {'type': 'string'}
+                        value = request_form.getlist(k)
+                else:
+                    if v.get('format') == 'binary':
+                        # FileStorage
+                        value = request_files.get(k)
+                    else:
+                        # str, int ...
+                        value = request_form.get(k)
+                if value is not None:
+                    form_dict[k] = value
+            request_kwargs.update({"form": form(**form_dict)})
+        if body:
+            if body.__custom_root_type__:
+                # https://pydantic-docs.helpmanual.io/usage/models/#custom-root-types
+                body_ = body(
+                    __root__=request.get_json(silent=True) if request.get_json(silent=True) is not None else {})
+            else:
+                body_ = body(
+                    **request.get_json(silent=True) if request.get_json(silent=True) is not None else {})
+            request_kwargs.update({"body": body_})
+    except ValidationError as e:
+        response = make_response(e.json())
+        response.headers['Content-Type'] = 'application/json'
+        response.status_code = 422
+        return response
+
+    # handle request
+    response = func(**request_kwargs)
+
+    VALIDATE_RESPONSE = current_app.config.get("VALIDATE_RESPONSE", False)
+    if VALIDATE_RESPONSE and responses:
+        validate_response(response, responses)
+
+    return response
