6fde5cdc8a0aa57d1da871366237e007b997ed76
luolingchun <luolingchun@outlook.com>
2022-04-13 16:42:15 +0800

    add description for UnprocessableEntity

--- flask_openapi3/models/validation_error.py
+++ flask_openapi3/models/validation_error.py
@@ -7,7 +7,12 @@ from pydantic import BaseModel, Field
 
 
 class UnprocessableEntity(BaseModel):
-    loc: Optional[List[str]] = Field(None, title="Location")
-    msg: Optional[str] = Field(None, title="Message")
-    type_: Optional[str] = Field(None, title="Error Type")
-    ctx: Optional[Dict[str, Any]] = Field(None, title="Error context")
+    # More information: https://pydantic-docs.helpmanual.io/usage/models/#error-handling
+    loc: Optional[List[str]] = Field(None, title="Location", description="the error's location as a list. ")
+    msg: Optional[str] = Field(None, title="Message", description="a computer-readable identifier of the error type.")
+    type_: Optional[str] = Field(None, title="Error Type", description="a human readable explanation of the error.")
+    ctx: Optional[Dict[str, Any]] = Field(
+        None,
+        title="Error context",
+        description="an optional object which contains values required to render the error message."
+    )
