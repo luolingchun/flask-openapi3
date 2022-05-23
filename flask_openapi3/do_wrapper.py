db2dd10b594fbd61ba6bad57490cc21364d3b4c7
luolingchun <luolingchun@outlook.com>
2022-05-23 09:44:18 +0800

    [#26] fix: Body throws exception when receiving str instead of dict

--- flask_openapi3/do_wrapper.py
+++ flask_openapi3/do_wrapper.py
@@ -1,13 +1,91 @@
 # -*- coding: utf-8 -*-
 # @Author  : llc
 # @Time    : 2022/4/1 16:54
+import json
+from json import JSONDecodeError
 from typing import Dict, Any, Type, Callable
 
 from flask import request, make_response, current_app
 from flask.wrappers import Response
 from pydantic import ValidationError, BaseModel
+from pydantic.error_wrappers import ErrorWrapper
 
 from .utils import validate_response
+
+
+def _do_header(header, request_kwargs):
+    request_headers = dict(request.headers) or {}
+    for key, value in header.__annotations__.items():
+        key_title = key.title()
+        # add original key
+        if key_title in request_headers.keys():
+            request_headers[key] = request_headers[key_title]
+    request_kwargs.update({"header": header(**request_headers)})
+
+
+def _do_cookie(cookie, request_kwargs):
+    request_cookies = cookie(**request.cookies or {})
+    request_kwargs.update({"cookie": request_cookies})
+
+
+def _do_path(path, kwargs, request_kwargs):
+    request_path = path(**kwargs)
+    request_kwargs.update({"path": request_path})
+
+
+def _do_query(query, request_kwargs):
+    request_args = request.args
+    query_dict = {}
+    for k, v in query.schema().get('properties', {}).items():
+        if v.get('type') == 'array':
+            value = request_args.getlist(k)
+        else:
+            value = request_args.get(k)
+        if value is not None:
+            query_dict[k] = value
+    request_kwargs.update({"query": query(**query_dict)})
+
+
+def _do_form(form, request_kwargs):
+    request_form = request.form
+    request_files = request.files
+    form_dict = {}
+    for k, v in form.schema().get('properties', {}).items():
+        if v.get('type') == 'array':
+            items = v.get('items', {})
+            if items.get('type') == 'string' and items.get('format') == 'binary':
+                # List[FileStorage]
+                # {'title': 'Files', 'type': 'array', 'items': {'format': 'binary', 'type': 'string'}
+                value = request_files.getlist(k)
+            else:
+                # List[str], List[int] ...
+                # {'title': 'Files', 'type': 'array', 'items': {'type': 'string'}
+                value = request_form.getlist(k)
+        else:
+            if v.get('format') == 'binary':
+                # FileStorage
+                value = request_files.get(k)
+            else:
+                # str, int ...
+                value = request_form.get(k)
+        if value is not None:
+            form_dict[k] = value
+    request_kwargs.update({"form": form(**form_dict)})
+
+
+def _do_body(body, request_kwargs):
+    obj = request.get_json(silent=True) or {}
+    if isinstance(obj, str):
+        try:
+            obj = json.loads(obj)
+        except JSONDecodeError as e:
+            raise ValidationError([ErrorWrapper(e, loc='__root__')], body)
+    if body.__custom_root_type__:
+        # https://pydantic-docs.helpmanual.io/usage/models/#custom-root-types
+        body_ = body(__root__=obj)
+    else:
+        body_ = body(**obj)
+    request_kwargs.update({"body": body_})
 
 
 def _do_wrapper(
@@ -39,62 +117,17 @@ def _do_wrapper(
     request_kwargs = dict()
     try:
         if header:
-            request_headers = dict(request.headers) or {}
-            for key, value in header.__annotations__.items():
-                key_title = key.title()
-                # add original key
-                if key_title in request_headers.keys():
-                    request_headers[key] = request_headers[key_title]
-            request_kwargs.update({"header": header(**request_headers)})
+            _do_header(header, request_kwargs)
         if cookie:
-            request_cookies = cookie(**request.cookies or {})
-            request_kwargs.update({"cookie": request_cookies})
+            _do_cookie(cookie, request_kwargs)
         if path:
-            request_path = path(**kwargs)
-            request_kwargs.update({"path": request_path})
+            _do_path(path, kwargs, request_kwargs)
         if query:
-            request_args = request.args
-            query_dict = {}
-            for k, v in query.schema().get('properties', {}).items():
-                if v.get('type') == 'array':
-                    value = request_args.getlist(k)
-                else:
-                    value = request_args.get(k)
-                if value is not None:
-                    query_dict[k] = value
-            request_kwargs.update({"query": query(**query_dict)})
+            _do_query(query, request_kwargs)
         if form:
-            request_form = request.form
-            request_files = request.files
-            form_dict = {}
-            for k, v in form.schema().get('properties', {}).items():
-                if v.get('type') == 'array':
-                    items = v.get('items', {})
-                    if items.get('type') == 'string' and items.get('format') == 'binary':
-                        # List[FileStorage]
-                        # {'title': 'Files', 'type': 'array', 'items': {'format': 'binary', 'type': 'string'}
-                        value = request_files.getlist(k)
-                    else:
-                        # List[str], List[int] ...
-                        # {'title': 'Files', 'type': 'array', 'items': {'type': 'string'}
-                        value = request_form.getlist(k)
-                else:
-                    if v.get('format') == 'binary':
-                        # FileStorage
-                        value = request_files.get(k)
-                    else:
-                        # str, int ...
-                        value = request_form.get(k)
-                if value is not None:
-                    form_dict[k] = value
-            request_kwargs.update({"form": form(**form_dict)})
+            _do_form(form, request_kwargs)
         if body:
-            if body.__custom_root_type__:
-                # https://pydantic-docs.helpmanual.io/usage/models/#custom-root-types
-                body_ = body(__root__=request.get_json(silent=True) or {})
-            else:
-                body_ = body(**request.get_json(silent=True) or {})
-            request_kwargs.update({"body": body_})
+            _do_body(body, request_kwargs)
     except ValidationError as e:
         response = make_response(e.json())
         response.headers['Content-Type'] = 'application/json'
---
+++ tests/test_str_body.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# @Author  : llc
+# @Time    : 2022/5/22 8:05
+
+import pytest
+from pydantic import BaseModel
+
+from flask_openapi3 import OpenAPI
+
+app = OpenAPI(__name__)
+app.config["TESTING"] = True
+
+
+class MyModel(BaseModel):
+    text: str
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+
+    return client
+
+
+@app.post('/path/')
+def create_book1(body: MyModel):
+    return body.text
+
+
+def test_post(client):
+    my_model = MyModel(text='1')
+    resp = client.post("/path/", json=my_model.json())
+    assert resp.status_code == 200
