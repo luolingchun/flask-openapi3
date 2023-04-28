895c1a39824fc0b0fcc3604041a862d14d1ad782
luolingchun <luolingchun@outlook.com>
2023-04-28 11:32:32 +0800

    update mkdocs.yml

--- docs/js/db.js
+++ docs/js/db.js
@@ -1,7 +1,5 @@
-var _hmt = _hmt || [];
-(function() {
-  var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?75ba4c3adb10ca83be9768ccb0713318";
-  var s = document.getElementsByTagName("script")[0]; 
-  s.parentNode.insertBefore(hm, s);
-})();
+(function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "gjkqgxq59d");
