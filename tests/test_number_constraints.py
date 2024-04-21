f048fb0a9e7ba76be8d74e2efc8ea81dd6cf9820
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2024-04-21 05:00:04 +0200

    Wrong types of exclusiveMinimum & exclusiveMaximum fields in Schema class (#149)
    * fix: change type of exclusiveMinimum & exclusiveMaximum from bool to float
    
    * test: add number constraints test

---
+++ tests/test_number_constraints.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+# @Author  : llc
+# @Time    : 2024/4/19 20:53
+
+import pytest
+from pydantic import BaseModel, Field
+
+from flask_openapi3 import OpenAPI
+
+app = OpenAPI(__name__)
+app.config["TESTING"] = True
+
+
+class MyModel(BaseModel):
+    num_1: int = Field(..., ge=1, le=10)
+    num_2: int = Field(..., gt=1, lt=10)
+
+
+@app.post('/book')
+def create_book(body: MyModel):
+    return body.dict()
+
+
+@pytest.fixture
+def client():
+    client = app.test_client()
+    return client
+
+
+def test_openapi(client):
+    resp = client.get("/openapi/openapi.json")
+    assert resp.status_code == 200
+    assert resp.json == app.api_doc
+
+    model_props = resp.json['components']['schemas']['MyModel']['properties']
+    num_1_props = model_props['num_1']
+    num_2_props = model_props['num_2']
+
+    assert num_1_props['minimum'] == 1
+    assert num_1_props['maximum'] == 10
+    assert num_2_props['exclusiveMinimum'] == 1
+    assert num_2_props['exclusiveMaximum'] == 10
