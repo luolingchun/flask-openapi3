370c631344f0b8a13297b668f414ba986d79968e
luolingchun <luolingchun@outlook.com>
2024-07-06 15:41:29 +0800

    Remove experimental export markdown

--- flask_openapi3/commands.py
+++ flask_openapi3/commands.py
@@ -7,16 +7,13 @@ from flask import current_app
 from flask.cli import click
 from flask.cli import with_appcontext
 
-from .markdown import openapi_to_markdown
-
 
 @click.command(name="openapi")
 @click.option("--output", "-o", type=click.Path(), help="The output file path.")
-@click.option("--format", "-f", type=click.Choice(["json", "yaml", "markdown"]), help="The output file format.")
+@click.option("--format", "-f", "_format", type=click.Choice(["json", "yaml"]), help="The output file format.")
 @click.option("--indent", "-i", type=int, help="The indentation for JSON dumps.")
-@click.option("--ensure_ascii", "-a", is_flag=True, help="Ensure ASCII characters or not. Defaults to False.")
 @with_appcontext
-def openapi_command(output, format, indent, ensure_ascii):
+def openapi_command(output, _format, indent):
     """Export the OpenAPI Specification to console or a file"""
 
     # Check if the current app has an api_doc attribute
@@ -24,16 +21,14 @@ def openapi_command(output, format, indent, ensure_ascii):
         obj = current_app.api_doc
 
         # Generate the OpenAPI Specification based on the specified format
-        if format == "yaml":
+        if _format == "yaml":
             try:
                 import yaml  # type: ignore
             except ImportError:
                 raise ImportError("pyyaml must be installed.")
             openapi = yaml.safe_dump(obj, allow_unicode=True)
-        elif format == "markdown":
-            openapi = openapi_to_markdown(obj)
         else:
-            openapi = json.dumps(obj, indent=indent, ensure_ascii=ensure_ascii)
+            openapi = json.dumps(obj, indent=indent, ensure_ascii=False)
 
         # Save the OpenAPI Specification to a file if the output path is provided
         if output:
