457c8dbd0cfe354b4e901ea438d110452f611f42
luolingchun <luolingchun@outlook.com>
2024-09-29 10:28:58 +0800

    Fix __get_pydantic_core_schema__

--- flask_openapi3/models/file.py
+++ flask_openapi3/models/file.py
@@ -14,14 +14,14 @@ class FileStorage(_FileStorage):
     """
 
     @classmethod
-    def __get_pydantic_json_schema__(cls, _core_schema, _handler) -> JsonSchemaValue:
+    def __get_pydantic_json_schema__(cls, *_args: Any, **_kwargs: Any) -> JsonSchemaValue:
         field_schema = {"format": "binary", "type": "string"}
         return field_schema
 
     @classmethod
-    def __get_pydantic_core_schema__(cls, _source: Any) -> core_schema.CoreSchema:
+    def __get_pydantic_core_schema__(cls, *_args: Any, **_kwargs: Any) -> core_schema.CoreSchema:
         return core_schema.with_info_plain_validator_function(cls.validate)
 
     @classmethod
-    def validate(cls, value: _FileStorage, _info: core_schema.ValidationInfo) -> _FileStorage:
+    def validate(cls, value: _FileStorage, *_args: Any, **_kwargs: Any) -> _FileStorage:
         return value
