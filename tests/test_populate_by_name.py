f434e14ca772f12b988d5b61234e025b5bc1b5f8
luolingchun <luolingchun@outloook.com>
2024-10-05 13:45:08 +0800

    Fix alias in query and form

--- tests/test_populate_by_name.py
+++ tests/test_populate_by_name.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # @Author  : llc
 # @Time    : 2024/8/31 15:35
+from typing import Sequence
+
 import pytest
 from pydantic import BaseModel, Field
 
@@ -29,17 +31,101 @@ def get_book(query: BookQuery):
     """
     get all books
     """
-    return {
-        "code": 0,
-        "message": "ok",
-        "data": [
-            {"bid": 1, "age": query.age, "author": query.author},
-            {"bid": 2, "age": query.age, "author": query.author}
-        ]
-    }
+    print(query)
+    return "ok"
 
 
-def test_openapi(client):
+class QueryModel(BaseModel):
+    aliased_field: str = Field(alias="aliasedField")
+
+    aliased_list_field: list[str] = Field(alias="aliasedListField")
+
+
+@app.get("/query-alias-test")
+def query_alias_test(query: QueryModel):
+    assert query.aliased_field == "test"
+    return "ok"
+
+
+class HeaderModel(BaseModel):
+    Hello1: str = Field(..., alias="Hello2")
+
+    model_config = {"populate_by_name": True}
+
+
+@app.get("/header")
+def get_book_header(header: HeaderModel):
+    return header.model_dump(by_alias=True)
+
+
+class TupleModel(BaseModel):
+    values: tuple[int, int]
+    sequence: Sequence[int] = Field(alias="Sequence")
+
+    model_config = {"populate_by_name": True}
+
+
+@app.get("/tuple-test")
+def tuple_test(query: TupleModel):
+    assert query.values == (2, 2)
+    return b"", 200
+
+
+class AliasModel(BaseModel):
+    aliased_field: str = Field(alias="aliasedField")
+
+
+@app.post("/form-alias-test")
+def alias_test(form: AliasModel):
+    assert form.aliased_field == "test"
+    return b"", 200
+
+
+def test_header(client):
+    headers = {"Hello2": "111"}
+    resp = client.get("/header", headers=headers)
+    print(resp.json)
+    assert resp.status_code == 200
+    assert resp.json == headers
+
+
+def test_tuple_query(client):
+    resp = client.get(
+        "/tuple-test",
+        query_string={"values": [2, 2], "sequence": [1, 2, 3]},
+    )
+    assert resp.status_code == 200
+
+
+def test_form_alias(client):
+    resp = client.post(
+        "/form-alias-test",
+        data={"aliasedField": "test"},
+    )
+    assert resp.status_code == 200
+
+    resp = client.post(
+        "/form-alias-test",
+        data={"aliased_field": "test"},
+    )
+    assert resp.status_code == 422
+
+
+def test_query_alias(client):
+    resp = client.get(
+        "/query-alias-test",
+        query_string={"aliasedField": "test", "aliasedListField": ["test"]},
+    )
+    assert resp.status_code == 200
+
+    resp = client.get(
+        "/query-alias-test",
+        data={"aliased_field": "test", "aliased_list_field": ["test"]},
+    )
+    assert resp.status_code == 422
+
+
+def test_query_populate_by_name(client):
     resp = client.get("/book?age=1&author=aa")
     assert resp.status_code == 200
     resp = client.get("/book?age=1&author_name=aa")
